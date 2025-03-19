8e699b0596c2f78c4fce448b78479bedc739f2e6
nidalass <leeshyeon@gmail.com>
2025-03-20 04:03:48 +0900

    Update App.js: Connect to backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "tailwindcss/tailwind.css";
 
-function App() {
+export default function HomePage() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/`)
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error("API 호출 오류:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-400 mb-4">AI 법률 지원</h1>
+      <p>{message}</p>
     </div>
   );
 }
-
-export default App;
